81374180098084ca746548ccd5760ceebcc3c966
dyoun57 <97987682+dyoun57@users.noreply.github.com>
2022-01-20 20:59:41 -0600

    Add files via upload

---
+++ Example 2/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800, 900);
+}
+
+function draw() {
+  
+
+  fill(255, 173, 183, 110);
+  circle(425,250,400);
+
+  fill(189, 173, 255, 200);
+  circle(250,450,400);
+
+  fill(173, 255, 206, 90);
+  circle(575,450,400);
+  
+  
+  
+}
